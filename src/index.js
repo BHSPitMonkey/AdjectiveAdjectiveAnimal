20efa2564f2f0329d0e7c04aa9460841411e892a
Stephen Eisenhauer <bhs2007@gmail.com>
2023-11-25 17:10:36 -0800

    Copy to clipboard on click

--- src/index.js
+++ src/index.js
@@ -1,13 +1,33 @@
-import { generateCombination } from 'gfycat-style-urls';
+import { generateCombination } from "gfycat-style-urls";
+
+function clickHandler(event) {
+  navigator.clipboard.writeText(event.target.innerText).then(
+    () => {
+      console.log("Copied to clipboard");
+      event.target.classList.add('copied');
+      setTimeout(() => {
+        event.target.classList.remove('copied');
+      }, 5000);
+    },
+    () => {
+      console.error("Failed to copy to clipboard");
+    }
+  );
+}
 
 function populateList() {
-    const list = document.getElementById('list');
+  const list = document.getElementById("list");
+  const size = 36;
+  list.innerHTML = '';
 
-    for (let i=0; i<36; i++) {
-        let li = document.createElement('li');
-        li.innerText = generateCombination(2, '', true);
-        list.appendChild(li);
-    }
+  for (let i = 0; i < size; i++) {
+    let li = document.createElement("li");
+    li.innerText = generateCombination(2, "", true);
+    li.addEventListener('click', clickHandler);
+    list.appendChild(li);
+  }
 }
 
 populateList();
+
+window.populateList = populateList;
